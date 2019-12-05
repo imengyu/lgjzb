e16ca58f80eeabfbbc0ab6d1ebf6c3d0955fb010
717021 <32213395+717021@users.noreply.github.com>
2019-12-05 14:38:52 +0800

    f

---
+++ server/conf/constConfTemplate.js
@@ -0,0 +1,15 @@
+const APPID = 'wxbbac277cde344ade';
+const APPID_SECRET = '691e2ba3d9821fc41e97e1c96211d5d4';
+
+module.exports = {
+  APPID: APPID,
+  APPID_SECRET: APPID_SECRET,
+  AUTH_CRYPT_KEY: 'rvpf6qpl1hb8g3r0',
+  AUTH_CRYPT_IV: 'emgrjcwqw07haf45',
+
+  DB_HOST: 'localhost',
+  DB_PORT: '3306',
+  DB_DATABASE: 'jzapi',
+  DB_USER: 'jzapi',
+  DB_PASSWORD: 'jzapi',
+}
