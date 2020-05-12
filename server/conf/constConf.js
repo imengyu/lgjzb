b11c2f8457e990b5969c1272cde97148818e039d
梦欤 <1501076885@qq.com>
2020-05-12 16:52:38 +0800

    a

---
+++ server/conf/constConf.js
@@ -0,0 +1,18 @@
+const APPID = 'wxbbac277cde344ade';
+const APPID_SECRET = '691e2ba3d9821fc41e97e1c96211d5d4';
+
+module.exports = {
+  APPID: APPID,
+  APPID_SECRET: APPID_SECRET,
+  AUTH_CRYPT_KEY: 'rvpf6qpl1hb8g3r0',
+  AUTH_CRYPT_IV: 'emgrjcwqw07haf45',
+
+  DB_HOST: 'localhost',
+  DB_PORT: '3306',
+  DB_DATABASE: 'jzapi',
+  DB_USER: 'jzapi',
+  DB_PASSWORD: 'jzapi',
+  REDIS_HOST: 'localhost',
+  REDIS_PORT: '6379',
+  REDIS_PASSWORD: '',
+}
